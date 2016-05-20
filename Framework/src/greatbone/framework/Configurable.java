d58c55b93f6c66424370bdfcb2fc28e87b30255e
hismichael <hismichael@qq.com>
2016-05-20 16:50:40 +0800

    cfg

--- Framework/src/greatbone/framework/Configurable.java
+++ Framework/src/greatbone/framework/Configurable.java
@@ -3,6 +3,7 @@ package greatbone.framework;
 import org.w3c.dom.Element;
 
 /**
+ * A configurable component that is associated with an configurative XML element.
  */
 public interface Configurable {
 
