8e0467dafc4631f5a1172a13821d991c7195816f
hismichael <hismichael@qq.com>
2016-05-31 15:28:55 +0800

    webprint

--- main/src/io/greatbone/web/WebContext.java
+++ main/src/io/greatbone/web/WebContext.java
@@ -1,6 +1,5 @@
 package io.greatbone.web;
 
-import io.greatbone.Out;
 import io.greatbone.grid.GridData;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.PooledByteBufAllocator;
@@ -19,15 +18,11 @@ import java.util.TimeZone;
 /**
  * A web request/response exchange.
  */
-public class WebContext implements Out<WebContext>, AutoCloseable {
+public class WebContext implements AutoCloseable {
 
     static final Charset UTF_8 = Charset.forName("UTF-8");
 
     static final int MAX_BODY = 64 * 1024;
-
-    static final int OUTBUF_INITIAL = 4 * 1024;
-
-    static final int OUTBUF_MAX = 64 * 1024;
 
     // standard HTTP date format
     static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
@@ -36,17 +31,6 @@ public class WebContext implements Out<WebContext>, AutoCloseable {
         TimeZone GMT_ZONE = TimeZone.getTimeZone("GMT");
         DATE_FORMAT.setTimeZone(GMT_ZONE);
     }
-
-    // possible chars for representing a number
-    static final char[] DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
-    static final char MINUS = '-';
-
-    static final short[] SHORTS = {10, 100, 1000, 10000};
-
-    static final int[] INTS = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 1000000000};
-
-    static final long[] LONGS = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 1000000000, 100000000000L, 1000000000000L, 10000000000000L, 100000000000000L, 100000000000000L, 10000000000000000L, 100000000000000000L, 1000000000000000000L};
 
     final WebHost host;
 
@@ -66,21 +50,11 @@ public class WebContext implements Out<WebContext>, AutoCloseable {
     // converted request content: form-data, json-deserilized and input stream
     Object content;
 
-    // the output buffer
-    ByteBuf outbuf;
-
     WebContext(WebHost host, ChannelHandlerContext chctx, FullHttpRequest req) {
         this.host = host;
         this.chctx = chctx;
         this.request = req;
         this.requesth = req.headers();
-    }
-
-    ByteBuf outbuf(boolean alloc) {
-        if (outbuf == null && alloc) {
-            outbuf = PooledByteBufAllocator.DEFAULT.buffer(OUTBUF_INITIAL, OUTBUF_MAX);
-        }
-        return outbuf;
     }
 
     public HttpMethod method() {
@@ -130,158 +104,6 @@ public class WebContext implements Out<WebContext>, AutoCloseable {
         return null;
     }
 
-    //
-    // OUTPUT FUNCTIONS
-
-    void write(char c) throws IOException {
-
-        ByteBuf buf = outbuf;
-        if (buf == null) {
-            buf = outbuf = PooledByteBufAllocator.DEFAULT.buffer(OUTBUF_INITIAL, OUTBUF_MAX);
-        }
-
-        // UTF-8 encoding but without surrogate support
-        if (c < 0x80) {
-            // have at most seven bits
-            buf.writeByte((byte) c);
-        } else if (c < 0x800) {
-            // 2 text, 11 bits
-            buf.writeByte((byte) (0xc0 | (c >> 6)));
-            buf.writeByte((byte) (0x80 | (c & 0x3f)));
-        } else {
-            // 3 text, 16 bits
-            buf.writeByte((byte) (0xe0 | ((c >> 12))));
-            buf.writeByte((byte) (0x80 | ((c >> 6) & 0x3f)));
-            buf.writeByte((byte) (0x80 | (c & 0x3f)));
-        }
-    }
-
-    public WebContext $(CharSequence v) throws IOException {
-        if (v != null) {
-            for (int i = 0; i < v.length(); i++) {
-                write(v.charAt(i));
-            }
-        }
-        return this;
-    }
-
-    @Override
-    public WebContext $(boolean v) throws IOException {
-        return this;
-    }
-
-    public WebContext $(short v) throws IOException {
-        int x = v;
-        if (v < 0) {
-            write(MINUS);
-            x = -v;
-        }
-        boolean begun = false;
-        for (int i = SHORTS.length - 1; i >= 0; i--) {
-            short base = SHORTS[i];
-            int q = x / base;
-            x = x % base;
-            if (q != 0 || begun) {
-                write(DIGITS[q]);
-                begun = true;
-            }
-        }
-        write(DIGITS[x]); // last reminder
-        return this;
-    }
-
-    public WebContext $(int v) throws IOException {
-        int x = v;
-        if (v < 0) {
-            write(MINUS);
-            x = -v;
-        }
-        boolean begun = false;
-        for (int i = INTS.length - 1; i >= 0; i--) {
-            int base = INTS[i];
-            int q = x / base;
-            x = x % base;
-            if (q != 0 || begun) {
-                write(DIGITS[q]);
-                begun = true;
-            }
-        }
-        write(DIGITS[x]); // last reminder
-        return this;
-    }
-
-    public WebContext $(long v) throws IOException {
-        long x = v;
-        if (v < 0) {
-            write(MINUS);
-            x = -v;
-        }
-        boolean begun = false;
-        for (int i = LONGS.length - 1; i >= 0; i--) {
-            long base = LONGS[i];
-            int q = (int) (x / base);
-            x = x % base;
-            if (q != 0 || begun) {
-                write(DIGITS[q]);
-                begun = true;
-            }
-        }
-        write(DIGITS[(int) x]); // last reminder
-        return this;
-    }
-
-    public WebContext $pair(String name, String value) throws IOException {
-        write('"');
-        $(name);
-        write('"');
-        write(':');
-        if (value == null) {
-            $("null");
-        } else {
-            write('"');
-            for (int i = 0; i < value.length(); i++) {
-                char c = value.charAt(i);
-                switch (c) {
-                    case '"':
-                        write('\\');
-                        write('"');
-                        break;
-                    case '\\':
-                        write('\\');
-                        write('\\');
-                        break;
-                    case '/':
-                        write('\\');
-                        write('/');
-                        break;
-                    case '\b':
-                        write('\\');
-                        write('b');
-                        break;
-                    case '\f':
-                        write('\\');
-                        write('f');
-                        break;
-                    case '\n':
-                        write('\\');
-                        write('n');
-                        break;
-                    case '\r':
-                        write('\\');
-                        write('r');
-                        break;
-                    case '\t':
-                        write('\\');
-                        write('t');
-                        break;
-                    default:
-                        write(c);
-                }
-            }
-            write('"');
-        }
-        return this;
-    }
 
     public WebContext $obj(GridData dat) {
 
@@ -308,17 +130,24 @@ public class WebContext implements Out<WebContext>, AutoCloseable {
 
     PooledByteBufAllocator pool;
 
+
+    public void sendOK(ByteBuf buf) throws IOException {
+        HttpHeaders headers = new DefaultHttpHeaders();
+        FullHttpResponse resp = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, buf, headers, new DefaultHttpHeaders());
+        chctx.writeAndFlush(resp);
+    }
+
     public void sendOK(WebPrint print) throws IOException {
 
         // print content to the out
-        print.wctx = this;
         print.print();
 
         HttpHeaders headers = new DefaultHttpHeaders();
         headers.set(HttpHeaderNames.CONTENT_TYPE, print.ctype());
-        headers.set(HttpHeaderNames.CONTENT_LENGTH, outbuf == null ? 0 : outbuf.readableBytes());
+        ByteBuf buf = print.buf;
+        headers.set(HttpHeaderNames.CONTENT_LENGTH, buf == null ? 0 : buf.readableBytes());
 
-        FullHttpResponse resp = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, outbuf, headers, new DefaultHttpHeaders());
+        FullHttpResponse resp = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, buf, headers, new DefaultHttpHeaders());
         chctx.writeAndFlush(resp);
     }
 
--- main/src/io/greatbone/web/WebPrint.java
+++ main/src/io/greatbone/web/WebPrint.java
@@ -1,6 +1,8 @@
 package io.greatbone.web;
 
 import io.greatbone.Out;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
 
 import java.io.IOException;
 
@@ -9,40 +11,125 @@ import java.io.IOException;
 @SuppressWarnings("unchecked")
 public abstract class WebPrint<O extends Out<O>> implements Out<O> {
 
-    // the wrapped output object
-    WebContext wctx;
+    static final int OUTBUF_INITIAL = 4 * 1024;
+
+    static final int OUTBUF_MAX = 64 * 1024;
+
+    // possible chars for representing a number
+    static final char[] DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    static final char MINUS = '-';
+
+    static final short[] SHORTS = {10, 100, 1000, 10000};
+
+    static final int[] INTS = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 1000000000};
+
+    static final long[] LONGS = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 1000000000, 100000000000L, 1000000000000L, 10000000000000L, 100000000000000L, 100000000000000L, 10000000000000000L, 100000000000000000L, 1000000000000000000L};
+
+    // the output buffer
+    ByteBuf buf;
 
     protected abstract String ctype();
 
     protected abstract void print() throws IOException;
 
+    //
+    // OUTPUT FUNCTIONS
+
+    void write(char c) throws IOException {
+
+        ByteBuf buf = this.buf;
+        if (buf == null) {
+            buf = this.buf = PooledByteBufAllocator.DEFAULT.buffer(OUTBUF_INITIAL, OUTBUF_MAX);
+        }
+
+        // UTF-8 encoding but without surrogate support
+        if (c < 0x80) {
+            // have at most seven bits
+            buf.writeByte((byte) c);
+        } else if (c < 0x800) {
+            // 2 text, 11 bits
+            buf.writeByte((byte) (0xc0 | (c >> 6)));
+            buf.writeByte((byte) (0x80 | (c & 0x3f)));
+        } else {
+            // 3 text, 16 bits
+            buf.writeByte((byte) (0xe0 | ((c >> 12))));
+            buf.writeByte((byte) (0x80 | ((c >> 6) & 0x3f)));
+            buf.writeByte((byte) (0x80 | (c & 0x3f)));
+        }
+    }
+
+    public O $(CharSequence v) throws IOException {
+        if (v != null) {
+            for (int i = 0; i < v.length(); i++) {
+                write(v.charAt(i));
+            }
+        }
+        return (O) this;
+    }
+
     @Override
     public O $(boolean v) throws IOException {
-        wctx.$(v);
         return (O) this;
     }
 
-    @Override
     public O $(short v) throws IOException {
-        wctx.$(v);
+        int x = v;
+        if (v < 0) {
+            write(MINUS);
+            x = -v;
+        }
+        boolean begun = false;
+        for (int i = SHORTS.length - 1; i >= 0; i--) {
+            short base = SHORTS[i];
+            int q = x / base;
+            x = x % base;
+            if (q != 0 || begun) {
+                write(DIGITS[q]);
+                begun = true;
+            }
+        }
+        write(DIGITS[x]); // last reminder
         return (O) this;
     }
 
-    @Override
     public O $(int v) throws IOException {
-        wctx.$(v);
+        int x = v;
+        if (v < 0) {
+            write(MINUS);
+            x = -v;
+        }
+        boolean begun = false;
+        for (int i = INTS.length - 1; i >= 0; i--) {
+            int base = INTS[i];
+            int q = x / base;
+            x = x % base;
+            if (q != 0 || begun) {
+                write(DIGITS[q]);
+                begun = true;
+            }
+        }
+        write(DIGITS[x]); // last reminder
         return (O) this;
     }
 
-    @Override
     public O $(long v) throws IOException {
-        wctx.$(v);
-        return (O) this;
-    }
-
-    @Override
-    public O $(CharSequence v) throws IOException {
-        wctx.$(v);
+        long x = v;
+        if (v < 0) {
+            write(MINUS);
+            x = -v;
+        }
+        boolean begun = false;
+        for (int i = LONGS.length - 1; i >= 0; i--) {
+            long base = LONGS[i];
+            int q = (int) (x / base);
+            x = x % base;
+            if (q != 0 || begun) {
+                write(DIGITS[q]);
+                begun = true;
+            }
+        }
+        write(DIGITS[(int) x]); // last reminder
         return (O) this;
     }
 
